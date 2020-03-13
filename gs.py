5c897cb85c005202fefeb6c3a41fa59e34e7fc75
code <33802433+codeacade@users.noreply.github.com>
2020-03-13 18:35:10 +0000

    problem with printing tuple

---
+++ gs.py
@@ -0,0 +1,12 @@
+# Gaussian distribution
+
+def randsteps(n):
+  import random
+  return sum([random.choice((-1,0,1)) for i in range(n)])
+  
+# Final counting with orinting. "n" for number of random steps, "m" for number of overall repiting
+  
+def distrib(n,m):
+  allr = [randsteps(n) for i in range(m)]  #randsteps(n) from above
+  for i in range(min(allr), max(allr)+1):
+    print(str(i),"\t",str("|"*allrcount*i)))

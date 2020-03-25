9c9d4acfc6018ad656be88ea18fae31f5628f58c
code <33802433+codeacade@users.noreply.github.com>
2020-03-25 23:51:25 +0000

    comments added

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,11 +1,16 @@
 import random
 
+# Random monoaplhabetic cipher generator
+# Randomly change order of 26 letters (arr[] -> brr[])
 def rans():
   arr = [i for i in "abcdefghijklmnoprstuwxyz"]
   brr = []
   print ("".join(arr)).upper()
-  while arr:
-    aa = random.choice(arr)
-    brr.append(aa)
-    arr.remove(aa)
+  
+  # ----------------------------program:
+  while arr:                  # as long as arr[] is not empty...
+    aa = random.choice(arr)   # select radon element of arr[]...
+    brr.append(aa)            # add it to brr[]...
+    arr.remove(aa)            # and remove it from arr[]
   print ("".join(brr)).upper() 
+# how to use it?...

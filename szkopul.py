aca1ab470a6676b1b0fb37d909d178849ccf83be
code <33802433+codeacade@users.noreply.github.com>
2020-04-09 00:37:47 +0100

    first exercuse from szkopul

---
+++ szkopul.py
@@ -0,0 +1,5 @@
+# more exercices from szkopul.edu.pl
+# count a number of letters in random string
+
+def lettercount(rstring):
+ return {i:rsting.count(i) for i in rstring}  # iterate over the string and count number of each character

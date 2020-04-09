8a2d9d78a9af1e9d327003b12b8aba7efbdc18f4
code <33802433+codeacade@users.noreply.github.com>
2020-04-09 23:44:42 +0100

    xml parser added

--- szkopul.py
+++ szkopul.py
@@ -3,3 +3,25 @@
 
 def lettercount(rstring):
  return {i:rsting.count(i) for i in rstring}  # iterate over the string and count number of each character
+
+
+# 3-level XML file parser using ElementTree:
+
+def xmlparse(xmlfile):
+  import xml.etree.ElementTree as et # ElemtTree required library
+  
+  rr = et.parse(xmlfile).getroot()
+  
+  for i in rr:
+    print(i.tag + " - " + i.text)
+    if len(i):
+      for j in i:
+        print("-" + j.tag + " - " + j.text)
+        if len(j):
+         for k in j:
+           print("--" + k.tag + " - " + k.text)
+        
+    
+    
+
+

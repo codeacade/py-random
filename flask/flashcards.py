b3b6e119d0a413c2badf3e3d4b598b08ad683aad
code <33802433+codeacade@users.noreply.github.com>
2020-04-23 00:48:24 +0100

    comments added

--- flask/flashcards.py
+++ flask/flashcards.py
@@ -1,3 +1,8 @@
+# 1.create virtual envoroment: $virtualenv name
+# 2.activate virtual enviroment: $source bin/bin/activate
+# 3.install Flask in new enviroment: $pip install -p python3 
+# 4.create py file in virtual enviroment
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,3 +18,8 @@ def counter():
   global cou
   cou+=1
   return str(cou)
+
+# 5.create enviromental variables (Debian):
+# 5.a $export FLASK_APP=flashcards.py   #no spaces next to "="
+# 5.b $export FLASK_ENV=development     #for easier debuging
+# 6. $flask run --host 0

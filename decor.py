31b6f3e8e57817b7a361734d6d42d5c8956a58c0
code <33802433+codeacade@users.noreply.github.com>
2021-02-26 00:23:11 +0000

    extended decorator definition

--- decor.py
+++ decor.py
@@ -1,6 +1,6 @@
 ##  hyperskills decorators explained
 #######################################
-##  PART 1.
+##  PART 1.Declaration
 
 def our_decorator(other_func):
   def wrapper(args_for_function):
@@ -9,7 +9,7 @@ def our_decorator(other_func):
   return wrapper
 
 #######################################
-##  PART 2.
+##  PART 2.Execution
 
 @our_decorator
 def greet(name):
@@ -22,15 +22,21 @@ greet("Suzie")  # this will return two lines:
 ######################################### END
 
 ##  DECORATORS FOR DUMMIES ##
-##  PART 1.
+##  PART 1.Declaration
 
 def this_is_decorator(pass_function_here):
   def this_is_only_wrapper(pass_arguments_pattern_here):  #  safe arguments (*x)
     print("This is part from Deccorator")           #  any Decorator content here
+    
     return pass_function_here(pass_arguments_pattern_here)  # THIS IS IMPORTANT
+    # "decorated" function can be returned like above
+    pass_function_here(pass_arguments_pattern_here)   #  THIS IS ALSO IMPORTANT
+    # "decorated" function can be launched like above
+  
+    peinr("THis is more from Decorator")    #  Decorator content after "decorated" function
   return this_is_only_wrapper        #  REMEMBER NO PARHENTASIS ()-NO
 
-##  PART 2.
+##  PART 2.Execution
 
 @this_is_decorator                    #  from the first line of PART 1.
 def any_new_function(arguments):      #  IMPORTANT - this function will be "decorated"

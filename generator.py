82c4d0bf8b1da402129f1d9e226657f646d40f21
code <33802433+codeacade@users.noreply.github.com>
2021-04-05 00:16:50 +0100

    copy of generator.py

---
+++ generator.py
@@ -0,0 +1,22 @@
+##########  YIELD / generators
+################################
+
+##  1.generate iterable function
+
+def fun(x):
+  for i in range(x):
+    yield i * 10
+    
+##  2.call function ITERABLE way
+
+print(*fun(7))    ##  print with star (*) operator
+
+list(fun(7))      ##  turn it into list
+
+## use next() to iterate 
+
+ff = fun(10)
+print(next(ff))    ##  returns 0
+print(next(ff))    ##  returns 10
+print(next(ff))    ##  returns 20
+print(next(ff))    ##  etc...

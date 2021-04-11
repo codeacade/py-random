b684b08b2b67a4fb7c08d7496bdf5382d84cd924
code <33802433+codeacade@users.noreply.github.com>
2021-04-11 23:57:27 +0100

    for record

---
+++ django/chat/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+##  MESSAGE ADDING PAGE FROM 101(8.1.12)
+
+##  MAIN CHAT PAGE WITH LOGIN FORM LINK
+def index(request):
+    return render(request, 'index.html')
+
+##  LOGIN PAGE FROM 101(8.1.11)
+def log_in(request):
+    #    return redirect('./alert')
+    log_info = 'Please type username and password'
+    if request.method == 'POST':
+        log_info = f"{request.POST.get('username')} not valid"
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            print("----VALID----")
+            login(request, form.get_user())
+            
+            return redirect('./')
+    
+    return render(request, 'login.html', {'form': AuthenticationForm(), 'info': log_info})
+
+def alert(request):
+    return HttpResponse("ALERT")
+    

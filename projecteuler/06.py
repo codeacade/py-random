86994b329db9834f36c9b303027d5ef74d187c9c
code <33802433+codeacade@users.noreply.github.com>
2021-04-25 23:25:33 +0100

    Problem 6th (simple) solved

---
+++ projecteuler/06.py
@@ -0,0 +1,14 @@
+##  https://projecteuler.net/problem=6
+########################################################################################################
+##  The sum of the squares of the first ten natural numbers is 385
+##  The square of the sum of the first ten natural numbers is 3025
+##  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 2640
+##  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+##############################################################
+
+def projecteuler06(x):
+    r = range(x+1)
+    print(sum(i for i in r)**2 - sum(i**2 for i in r))
+
+##  projecteuler06(10) >>> 2640
+##  projecteuler06(100) >>> 25164150

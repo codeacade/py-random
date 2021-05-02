d56eeeecdedbf0799ee9e1f0868ecd135f545152
code <33802433+codeacade@users.noreply.github.com>
2021-05-02 21:47:45 +0100

    smallest divisible by 1-20

--- projecteuler/05.py
+++ projecteuler/05.py
@@ -1,8 +1,8 @@
 ##  https://projecteuler.net/problem=5
-########################################################################################################
+###########################################################################################################
 ##  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 ##  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-########################################################################################################
+###########################################################################################################
   
 def isPrime(x):                           ##  used in primesP() function below
     for i in range(2, int(x // 2) + 1):

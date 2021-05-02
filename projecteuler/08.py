485008672d5cde6e0953dda5a82cf48ca118a17c
code <33802433+codeacade@users.noreply.github.com>
2021-05-02 22:05:31 +0100

    Largest product of the thirteen adjacent digits

--- projecteuler/08.py
+++ projecteuler/08.py
@@ -1,5 +1,5 @@
 ##  https://projecteuler.net/problem=8
-########################################################################################################
+##########################################################################################################
 ##  The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 ##  Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 ##########################################################################################################

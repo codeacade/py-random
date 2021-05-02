8925e2661a450cb086723e3b6861a5e5aa96e9c2
code <33802433+codeacade@users.noreply.github.com>
2021-05-02 21:51:22 +0100

    Sum of the squares minus square of the sums

--- projecteuler/06.py
+++ projecteuler/06.py
@@ -4,7 +4,7 @@
 ##  The square of the sum of the first ten natural numbers is 3025
 ##  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 2640
 ##  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-##############################################################
+########################################################################################################
 
 def projecteuler06(x):
     r = range(x+1)

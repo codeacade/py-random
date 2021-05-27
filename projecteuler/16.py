b8ce9582bebbddebcb7be8645fd3293e71011ddd
code <33802433+codeacade@users.noreply.github.com>
2021-05-27 23:58:40 +0100

    Digits sum of 2 to power 1000

---
+++ projecteuler/16.py
@@ -0,0 +1,9 @@
+##  https://projecteuler.net/problem=16
+#########################################################################################
+##  2 to power 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+##  What is the sum of the digits of the number 2 to power 1000?
+#################################################################################
+
+sum(map(int,str(2**1000)))
+
+##  I am not sure if understand this problem correctly. It is very easy to calculate = 1366

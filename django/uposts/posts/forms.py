926571c8ec9502fc97c690c2bf904350deb77250
code <33802433+codeacade@users.noreply.github.com>
2021-05-28 18:53:28 +0100

    fix ModelForm error

--- django/uposts/posts/forms.py
+++ django/uposts/posts/forms.py
@@ -6,3 +6,4 @@ class PostForm(forms.ModelForm):
     class Meta:             #  specified fields of form
         model = Post
         field = ['title', 'image']
+        fields='__all__'  ## fix for "Creating a ModelForm without either the 'fields' attribute or the 'exclude' attribute is prohibited"
